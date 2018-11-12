c551b250ec9a50cf59461b64dfd2bc8a40b003e4
rikaririRirIKa <38421243+rikaririRirIKa@users.noreply.github.com>
2018-11-12 17:54:26 +0900

    Update default.js

--- default.js
+++ default.js
@@ -1,16 +1,16 @@
 //答えを用意
-var number=Math.floor(Math.random()*3);
-var anwer=parseInt(window.prompt('数当てゲーーーーーム！0~2の数字を入力してね'));
+var number = Math.floor(Math.random()*3);
+var anwer = parseInt(window.prompt('数当てゲーーーーーム！0~2の数字を入力してね'));
 
 var message;
-if(answer===number){
-   message='あたり！';
+if(answer === number){
+   message = 'あたり！';
   }else if(answer<number){
-   message='残念～もっと大きいよ！';
+   message = '残念～もっと大きいよ！';
   }else if(number<answer){
-   message='残念～もっと小さいよ！';
+   message = '残念～もっと小さいよ！';
   }else{
-   message='0~3の数字を入力してね';
+   message = '0~3の数字を入力してね';
   }
   
   //結果を表示
